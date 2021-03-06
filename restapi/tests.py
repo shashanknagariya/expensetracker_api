91b47d773694f5d7d687fc68c888335213ac506b
shashank <nagariya.shashank7@gmail.com>
2021-03-06 23:29:27 +0530

    To add testcases

--- restapi/tests.py
+++ restapi/tests.py
@@ -1,3 +1,12 @@
-from django.test import TestCase
+from unittest import TestCase
 
 # Create your tests here.
+
+
+def two_integer_sum(a, b):
+    return 0
+
+
+class testsum(TestCase):
+    def test_sum(self):
+        self.assertEqual(two_integer_sum(1, 2)), 3
